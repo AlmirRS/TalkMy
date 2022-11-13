4d077b93d84b15e17a453b240b306748c0d43210
Almir Ramos <93881716+AlmirRS@users.noreply.github.com>
2022-11-13 13:01:09 -0300

    atualização de projeto

--- app/src/main/java/com/baggold/net/talkmy/activityes/MainActivity.java
+++ app/src/main/java/com/baggold/net/talkmy/activityes/MainActivity.java
@@ -1,67 +1,140 @@
 package com.baggold.net.talkmy.activityes;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.baggold.net.talkmy.R;
+import com.baggold.net.talkmy.fragments.FeedFragment;
+import com.baggold.net.talkmy.fragments.PerfilFragment;
+import com.baggold.net.talkmy.fragments.PesquisarFragment;
+import com.baggold.net.talkmy.fragments.PostagensFragment;
+import com.baggold.net.talkmy.helper.ConfiguracaoFirebase;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //iniciar componentes
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
+        //Configura toolbar
         Toolbar toolbar = findViewById(R.id.toolbar_principal);
-        //toolbar.setTitle("Talk My");
+        toolbar.setTitle("Instagram");
         setSupportActionBar( toolbar );
 
-        iniciarBottomNavigation();
+        //configuracoes de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        //Configurar bottom navigation view
+        configuraBottomNavigationView();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(androidx.constraintlayout.widget.R.id.view_transition, new FeedFragment()).commit();
+
     }
 
-    private void iniciarBottomNavigation() {
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.bottom_nav);
-        assert navHostFragment != null;
-        NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    /**
+     * Método responsável por criar a BottomNavigation
+     */
+    private void configuraBottomNavigationView(){
+
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+
+        //faz configurações iniciais do Bottom Navigation
+        bottomNavigationViewEx.enableAnimation(true);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+
+
+        //Habilitar navegação
+        habilitarNavegacao( bottomNavigationViewEx );
+
+        //configura item selecionado inicialmente
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+    }
+
+
+    /**
+     * Método responsável por tratar eventos de click na BottomNavigation
+     */
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()){
+                    case R.id.ic_home :
+                        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+                        return true;
+                    case R.id.ic_pesquisa :
+                        fragmentTransaction.replace(R.id.viewPager, new PesquisarFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem :
+                        fragmentTransaction.replace(R.id.viewPager, new PostagensFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil :
+                        fragmentTransaction.replace(R.id.viewPager, new PerfilFragment()).commit();
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_up_principal, menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_up_principal, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.menu_sair){
-            FirebaseAuth.getInstance().signOut();
-
-            Toast.makeText(MainActivity.this,"Usuario deslogado!",Toast.LENGTH_SHORT).show();
-
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+        switch (item.getItemId()){
+            case R.id.menu_sair :
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
         }
+
         return super.onOptionsItemSelected(item);
     }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
